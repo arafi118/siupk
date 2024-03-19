3baf17e531cfafaf0fcb9b53476a275a567f09b6
nurfandi31 <fandinur241@gmail.com>
2024-03-19 12:21:48 +0700

    test

--- resources/views/_trigger.blade.php
+++ resources/views/_trigger.blade.php
@@ -57,18 +57,3 @@ kredit=(SELECT SUM(jumlah) FROM transaksi_1 WHERE rekening_kredit=OLD.rekening_k
 
 END$$
 DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER `update_saldo_debit` AFTER UPDATE ON `transaksi_1`
-FOR EACH ROW BEGIN
-
-INSERT INTO saldo (`id`, `kode_akun`, `lokasi`, `tahun`, `bulan`, `debit`, `kredit`)
-VALUES (CONCAT(REPLACE(NEW.rekening_debit, '.',''), 1, YEAR(NEW.tgl_transaksi), MONTH(NEW.tgl_transaksi)),
-NEW.rekening_debit, 1, YEAR(NEW.tgl_transaksi), MONTH(NEW.tgl_transaksi), (SELECT SUM(jumlah) FROM transaksi_1 WHERE
-rekening_debit=NEW.rekening_debit), (SELECT SUM(jumlah) FROM transaksi_1 WHERE rekening_debit=NEW.rekening_debit)) ON
-DUPLICATE KEY UPDATE
-debit= (SELECT SUM(jumlah) FROM transaksi_1 WHERE rekening_debit=NEW.rekening_debit),
-kredit=(SELECT SUM(jumlah) FROM transaksi_1 WHERE rekening_kredit=NEW.rekening_debit);
-
-END$$
-DELIMITER ;
