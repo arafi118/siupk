711947c13189b90089ebf1d16c0e5af40ad39496
nurfandi31 <fandinur241@gmail.com>
2024-07-25 13:17:52 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,32 +4,28 @@
 <head>
     <meta charset="utf-8">
     <meta name="description" content="Jembatan Akuntabilitas Bumdesma">
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="keywords"
-        content="dbm, sidbm, sidbm.net, demo.sidbm.net, app.sidbm.net, asta brata teknologi, abt, dbm, kepmendesa 136, kepmendesa nomor 136 tahun 2022">
+    <meta name="keywords" content="dbm, sidbm, sidbm.net, demo.sidbm.net, app.sidbm.net, asta brata teknologi, abt, dbm, kepmendesa 136, kepmendesa nomor 136 tahun 2022">
     <meta name="author" content="Enfii">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ $logo }}">
     <link rel="icon" type="image/png" href="{{ $logo }}">
     <title> Aplikasi LKM V.9.10</title>
 
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
-
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-
     <link id="pagestyle" href="/assets/css/material-dashboard.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="/assets/css/style.css" media="screen" title="no title">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <div class="login">
+</head>
 
+<body>
+    <div class="login">
         <div class="avatar">
             {{-- <img src="/assets/img/abt_logo.png" style="width: 100px;" alt="Avatar"> --}}
             <img src="{{ $logo }}" style="width: 100px;" alt="Avatar" />
-
         </div>
 
         <h2><b> {{ $kec->nama_lembaga_sort }}</b></h2>
@@ -52,74 +48,73 @@
         </form>
 
         <div class="bottom">
-            <a href="#">&copy; {{ date('Y') }} PT. Asta Brata
-                Teknologi &mdash; {{ str_pad($kec->id, 4, '0', STR_PAD_LEFT) }}</a>
+            <a href="#">&copy; {{ date('Y') }} PT. Asta Brata Teknologi &mdash; {{ str_pad($kec->id, 4, '0', STR_PAD_LEFT) }}</a>
         </div>
     </div>
-</head>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="/assets/js/core/popper.min.js"></script>
-<script src="/assets/js/core/bootstrap.min.js"></script>
-<script src="/assets/js/plugins/perfect-scrollbar.min.js"></script>
-<script src="/assets/js/plugins/smooth-scrollbar.min.js"></script>
-<script src="/assets/js/material-dashboard.min.js"></script>
-<script src="/assets/js/plugins/sweetalert.min.js"></script>
-<script>
-    if (localStorage.getItem('devops') !== 'true') {
-        $(document).bind("contextmenu", function(e) {
-            return false;
-        });
-
-        $(document).keydown(function(event) {
-            if (event.keyCode == 123) { // Prevent F12
-                return false;
-            }
-            if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-                return false;
-            }
-            if (event.ctrlKey && event.shiftKey && event.keyCode == 67) { // Prevent Ctrl+Shift+C  
-                return false;
-            }
-            if (event.ctrlKey && event.shiftKey && event.keyCode == 74) { // Prevent Ctrl+Shift+J
-                return false;
-            }
-        });
-    }
-</script>
-
-@if (session('pesan'))
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="/assets/js/core/popper.min.js"></script>
+    <script src="/assets/js/core/bootstrap.min.js"></script>
+    <script src="/assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="/assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="/assets/js/material-dashboard.min.js"></script>
+    <script src="/assets/js/plugins/sweetalert.min.js"></script>
     <script>
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-        })
+        if (localStorage.getItem('devops') !== 'true') {
+            $(document).bind("contextmenu", function(e) {
+                return false;
+            });
 
-        function Toastr(icon, text) {
-            font = "1.2rem Nimrod MT";
-
-            canvas = document.createElement("canvas");
-            context = canvas.getContext("2d");
-            context.font = font;
-            width = context.measureText(text).width;
-            formattedWidth = Math.ceil(width) + 100;
-
-            Toast.fire({
-                icon: icon,
-                title: text,
-                width: formattedWidth
-            })
+            $(document).keydown(function(event) {
+                if (event.keyCode == 123) { // Prevent F12
+                    return false;
+                }
+                if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+                    return false;
+                }
+                if (event.ctrlKey && event.shiftKey && event.keyCode == 67) { // Prevent Ctrl+Shift+C  
+                    return false;
+                }
+                if (event.ctrlKey && event.shiftKey && event.keyCode == 74) { // Prevent Ctrl+Shift+J
+                    return false;
+                }
+            });
         }
-
-        Toastr('success', "{{ session('pesan') }}")
     </script>
-@endif
+
+    @if (session('pesan'))
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            function Toastr(icon, text) {
+                font = "1.2rem Nimrod MT";
+
+                canvas = document.createElement("canvas");
+                context = canvas.getContext("2d");
+                context.font = font;
+                width = context.measureText(text).width;
+                formattedWidth = Math.ceil(width) + 100;
+
+                Toast.fire({
+                    icon: icon,
+                    title: text,
+                    width: formattedWidth
+                })
+            }
+
+            Toastr('success', "{{ session('pesan') }}")
+        </script>
+    @endif
+</body>
 
 </html>
