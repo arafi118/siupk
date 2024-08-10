bd15537f70bbb14502fb986252209b0f6e5ff107
arafi118 <arp46301@gmail.com>
2024-08-10 08:34:26 +0700

    App

--- routes/web.php
+++ routes/web.php
@@ -99,6 +99,7 @@ Route::group(['prefix' => 'kab', 'as' => 'kab.', 'middleware' => 'kab'], functio
 
 Route::get('/', [AuthController::class, 'index'])->middleware('guest')->name('/');
 Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::get('/app', [AuthController::class, 'app']);
 
 // Route::get('/force/{uname}', [AuthController::class, 'force'])->middleware('guest');
 
