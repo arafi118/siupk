27ee40a6aec32db4e4229754b97b0a790bec5ac6
nurfandi31 <fandinur241@gmail.com>
2024-09-03 09:57:01 +0700

    co ojk

--- resources/views/pelaporan/view/ojk/cover_o.blade.php
+++ resources/views/pelaporan/view/ojk/cover_o.blade.php
@@ -60,26 +60,54 @@
         <h1 style="margin: 0px;">{{ strtoupper($judul) }}</h1>
         <div style="margin: 0px; font-size: 24px;">{{ strtoupper($sub_judul) }}</div>
         <main><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-       <table width="50%" border="1" align="center"style="font-size: 13px;">
-        <tr>
-            <td align="center"colspan="2">Disusun Oleh</td>
-        </tr>
-        <tr>
-            <td colspan="">&nbsp;</td>
-        </tr>
-        <tr>
-            <td width="15">Ketua</td>
-            <td width="15" colspan="1">:{{$kec->sebutan_level_1}}</td>
-        </tr>
-        <tr>
-            <td width="15">Sekretaris</td>
-            <td width="30" colspan="1">:{{$kec->sebutan_level_2}}</td>
-        </tr>
-        <tr>
-            <td width="15">Bendahara</td>
-            <td width="15" colspan="1">:{{$kec->sebutan_level_3}}</td>
-        </tr>
-       </table>
+            @if($kec->id == 362)
+            <table width="50%" border="1" align="center" style="font-size: 13px;">
+                <tr>
+                    <td align="center" colspan="2">Disusun Oleh</td>
+                </tr>
+                <tr>
+                    <td colspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="15">Direktur utama </td>
+                    <td width="15" colspan="1">: Cicik Yuni Khustiyah</td>
+                </tr>
+                <tr>
+                    <td width="15">Direktur</td>
+                    <td width="30" colspan="1">: Fufut Widianita, ST</td>
+                </tr>
+                <tr>
+                    <td width="15">Kabag Administrasi </td>
+                    <td width="30" colspan="1">: Lailiyatus Sofiyah, SH</td>
+                </tr>
+                <tr>
+                    <td width="15">Kabag Keuangan </td>
+                    <td width="15" colspan="1">: Yernada Desi Kurnia Sari</td>
+                </tr><i class="g fa-github-alt    "></i>
+            </table>
+        @else
+        <table width="50%" border="0" align="center" style="font-size: 13px;">
+            <tr>
+                <td align="center" colspan="2">Disusun Oleh</td>
+            </tr>
+            <tr>
+                <td colspan="2">&nbsp;</td>
+            </tr>
+            <tr>
+                <td width="15">Ketua</td>
+                <td width="15" colspan="1">:{{$kec->sebutan_level_1}}</td>
+            </tr>
+            <tr>
+                <td width="15">Sekretaris</td>
+                <td width="30" colspan="1">:{{$kec->sebutan_level_2}}</td>
+            </tr>
+            <tr>
+                <td width="15">Bendahara</td>
+                <td width="15" colspan="1">:{{$kec->sebutan_level_3}}</td>
+            </tr>
+        </table>
+        @endif
+        
 
 </main>
     </header>
