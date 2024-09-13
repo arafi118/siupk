5d6e5a1562503d39c2f7f41f07d258e3dbc5af63
nurfandi31 <fandinur241@gmail.com>
2024-09-13 11:18:47 +0700

    main js

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -31827,7 +31827,7 @@ button,
   width: 330px;
 }
 .search-wrapper.active .input-holder {
-  width: 790px;
+  width: 740px;
   border-radius: 50px;
   background: rgba(0, 0, 0, 0.05);
   transition: all 0.5s cubic-bezier(0, 0.105, 0.035, 1.57);
@@ -31860,7 +31860,7 @@ button,
   transform: rotate(-45deg);
 }
 .search-wrapper.active .btn-close {
-  left: 793px;
+  left: 750px;
   opacity: 0.6 !important;
   transition: all 0.6s cubic-bezier(0, 0.105, 0.035, 1.57);
   transition-delay: 0.5s;
