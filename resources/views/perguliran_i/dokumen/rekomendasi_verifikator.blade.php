499afb4fa33ea6026bc1fce68964fb874f438b66
nurfandi31 <fandinur241@gmail.com>
2024-10-17 08:25:59 +0700

    rekomendasi kredit

--- resources/views/perguliran_i/dokumen/rekomendasi_verifikator.blade.php
+++ resources/views/perguliran_i/dokumen/rekomendasi_verifikator.blade.php
@@ -1,107 +1,14 @@
 @php
     use App\Utils\Tanggal;
 @endphp
+@extends('perguliran_i.dokumen.layout.base')
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ ucwords(str_replace('_', ' ', $judul)) }}</title>
-    <style>
-        * {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        html {
-            margin: 75.59px;
-            margin-left: 94.48px;
-        }
-
-        ul,
-        ol {
-            margin-left: -10px;
-            page-break-inside: auto !important;
-        }
-
-        header {
-            position: fixed;
-            top: -10px;
-            left: 0px;
-            right: 0px;
-        }
-
-        footer {
-            position: fixed;
-            bottom: -50px;
-            left: 0px;
-            right: 0px;
-        }
-
-        table tr th,
-        table tr td {
-            padding: 2px 4px;
-        }
-
-        table tr td table tr td {
-            padding: 0 !important;
-        }
-
-        .break {
-            page-break-after: always;
-        }
-
-        li {
-            text-align: justify;
-        }
-
-        .l {
-            border-left: 1px solid #000;
-        }
-
-        .t {
-            border-top: 1px solid #000;
-        }
-
-        .r {
-            border-right: 1px solid #000;
-        }
-
-        .b {
-            border-bottom: 1px solid #000;
-        }
-    </style>
-</head>
-
-<body>
-    <main>
-        <table border="0" width="100%" cellspacing="0" cellpadding="0"
-            style="font-size: 11px; position: relative; top: -20px;">
-            <tr class="b">
-                <td align="center">
-                    <img src="../storage/app/public/logo_kab/{{ $kab->id }}.png" width="70"
-                        alt="{{ $kab->id }}.png" style="margin-bottom: 8px;">
-                </td>
-                <td align="center">
-                    <div style="font-size: 18px;">
-                    {{ $kec->nama_lembaga_sort }} {{ strtoupper($nama_kabupaten) }}
-                    </div>
-                    <div style="font-size: 18px;">
-                        {{ strtoupper($kec->sebutan_kec) }} {{ strtoupper($kec->nama_kec) }}
-                    </div>
-                    <div align="center"style="font-size: 12px;">
-                    Alamat: {{ $kec->nama_lembaga_sort }}
-
-                      
-                    </div>
-                </td>
-            </tr>
-        </table>
-
+<br><br><br><br>
         <table border="0" width="85%" align="center"cellspacing="0" cellpadding="0" style="font-size: 12px;">
             <tr>
                 <td align="center">
                     <div style="font-size: 18px;">
-                        <b>REKOMENDASI HASIL VERIVIKASI/ANALISA KREDIT</b>
+                        <b>REKOMENDASI HASIL FERIVIKASI/ANALISA KREDIT</b>
                     </div>
                     <div style="font-size: 12px;">
                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   Nomor:..................................... &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -230,8 +137,7 @@
             <td width="200"align="center"colspan="1">&nbsp;</td>
                 <td align="center">
                     <u>
-                        <p>( {{$user->namadepan }} {{$user->namabelakang }} )<br>
-                    Ketua Tim
+                        <p> {{$user->namadepan }} {{$user->namabelakang }} <br> __________________________________<br>
                    </p>
                     </u>
                 </td>
