8766417e8d56f90729078a03d21b158d0ca1be92
yukaristel <rokumitsu012@gmail.com>
2025-01-09 10:02:25 +0700

    cek bkk

--- resources/views/transaksi/dokumen/bkk.blade.php
+++ resources/views/transaksi/dokumen/bkk.blade.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BUKTI KAS KELUAR</title>
     <style>
@@ -19,64 +17,47 @@
 
         .box {
             width: 14cm;
-            height: 9cm;
+            min-height: 9cm;
             border: 2px solid #000;
-            padding-top: 16px;
-            padding-bottom: 12px;
-            padding-right: 22px;
-            padding-left: 12px;
+            padding: 16px 22px 12px 12px;
+            display: flex;
+            flex-direction: column;
         }
 
         .box-header {
-            padding-left: 16px;
-            padding-right: 16px;
-            padding-bottom: 8px;
+            padding: 0 16px 8px 16px;
             border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+        }
+
+        .box-body {
+            flex: 1;
+            padding: 0 24px;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .content-section {
+            flex: 1;
+        }
+
+        .signature-section {
+            margin-top: auto;
+            padding-top: 20px;
+            padding-bottom: 10px;
         }
 
         .flex {
             display: flex;
         }
 
-        .block {
-            display: block;
-        }
-
-        .inline-block {
-            display: inline-block;
-        }
-
         .fw-bold {
             font-weight: bold;
         }
 
-        .fs-8 {
-            font-size: 8px;
-        }
+        .fs-10 { font-size: 10px; }
+        .fs-12 { font-size: 12px; }
 
-        .fs-10 {
-            font-size: 10px;
-        }
-
-        .fs-12 {
-            font-size: 12px;
-        }
-
-        .fs-14 {
-            font-size: 14px;
-        }
-
-        .fs-16 {
-            font-size: 16px;
-        }
-
-        .-mt-2 {
-            margin-top: -2px;
-        }
-
-        .ml-4 {
-            margin-left: 4px;
-        }
+        .ml-4 { margin-left: 4px; }
 
         .align-items-center {
             align-items: center;
@@ -86,116 +67,109 @@
             justify-content: space-between;
         }
 
-        .box-body {
-            padding-top: 0px;
-            padding-left: 24px;
-            padding-right: 24px;
-        }
-
         .keterangan {
             padding: 2px 4px;
             font-weight: normal;
         }
 
-        .jajargenjang {
-            background-color: rgba(0, 0, 0, 0.2);
-            -ms-transform: skew(-20deg);
-            -webkit-transform: skew(-20deg);
-            transform: skew(-20deg);
+        h1 {
             text-align: center;
+            font-size: 14px;
+            margin: 10px 0;
         }
 
-        .border-b {
-            border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+        table {
+            width: 100%;
         }
 
-        .text-left {
-            padding-left: 6px;
-            padding-right: 6px;
-            padding-top: 2px;
-            padding-bottom: 4px;
-            text-align: left;
+        .main-content td {
+            padding: 4px 0;
+        }
+
+        .signature-table td {
+            text-align: center;
+            padding: 4px 0;
+        }
+
+        .signature-space {
+            height: 60px;
         }
     </style>
 </head>
-
 <body onLoad="window.print()">
-    <div class="container">
-        <div class="box">
-            <div class="box-header flex align-items-center justify-content-between fs-10">
-                <div class="flex align-items-center">
-                    <img src="<?php echo $gambar; ?>" width="50" height="50">
-                    <div class="ml-4">
-                        <div class="block fw-bold">{{ strtoupper($kec->nama_lembaga_sort) }}</div>
-                        <div class="block fw-bold">
-                            {{ strtoupper('Kec. ' . $kec->nama_kec . ' Kab. ' . $kec->kabupaten->nama_kab . ' ' . $kec->kabupaten->nama_prov) }}
-                        </div>
-                        <div class="block fs-10">{{ 'SK Kemenkumham RI No. ' . $kec->nomor_bh }}</div>
-                        <div class="block fs-10">{{ $kec->alamat_kec . ', Telp. ' . $kec->telpon_kec }}</div>
+    <div class="box">
+        <div class="box-header flex align-items-center justify-content-between fs-10">
+            <div class="flex align-items-center">
+                <img src="<?php echo $gambar; ?>" width="50" height="50">
+                <div class="ml-4">
+                    <div class="block fw-bold">{{ strtoupper($kec->nama_lembaga_sort) }}</div>
+                    <div class="block fw-bold">
+                        {{ strtoupper('Kec. ' . $kec->nama_kec . ' Kab. ' . $kec->kabupaten->nama_kab . ' ' . $kec->kabupaten->nama_prov) }}
                     </div>
-                </div>
-                <div class="justify-right">
-                    <table>
-                        <tr>
-                            <td>Nomor</td>
-                            <td>:</td>
-                            <td><?php echo $trx->idt . '/BKK'; ?></td>
-                        </tr>
-                        <tr>
-                            <td>Tanggal</td>
-                            <td>:</td>
-                            <td>{{ Tanggal::tglIndo($trx->tgl_transaksi) }}</td>
-                        </tr>
-                    </table>
+                    <div class="block fs-10">{{ 'SK Kemenkumham RI No. ' . $kec->nomor_bh }}</div>
+                    <div class="block fs-10">{{ $kec->alamat_kec . ', Telp. ' . $kec->telpon_kec }}</div>
                 </div>
             </div>
-            <div class="box-body fs-12">
-                <table width="100%">
+            <div>
+                <table>
                     <tr>
-                        <td colspan="5" class="fs-10" align="center">
-                            <h1>BUKTI KAS KELUAR</h1>
-                        </td>
+                        <td>Nomor</td>
+                        <td>:</td>
+                        <td><?php echo $trx->idt . '/BKK'; ?></td>
                     </tr>
+                    <tr>
+                        <td>Tanggal</td>
+                        <td>:</td>
+                        <td>{{ Tanggal::tglIndo($trx->tgl_transaksi) }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="box-body fs-12">
+            <div class="content-section">
+                <h1>BUKTI KAS KELUAR</h1>
+                <table class="main-content">
                     <tr>
                         <td width="30%">Dibayar Kepada</td>
                         <td width="2%">:</td>
-                        @if ($trx->id_pinj > 0)
-                            <td colspan="3" class="keterangan">{{ ucwords('Kelompok ' . $trx->relasi) }}</td>
-                        @else
-                            <td colspan="3" class="keterangan">{{ ucwords($trx->relasi) }}</td>
-                        @endif
-                    </tr>
-                    <tr>
-                        <td width="30%">Keterangan</td>
-                        <td width="2%">:</td>
-                        <td colspan="3" class="keterangan">
-                            {{ ucwords($trx->keterangan_transaksi) }}
+                        <td class="keterangan">
+                            @if ($trx->id_pinj > 0)
+                                {{ ucwords('Kelompok ' . $trx->relasi) }}
+                            @else
+                                {{ ucwords($trx->relasi) }}
+                            @endif
                         </td>
                     </tr>
                     <tr>
-                        <td width="30%">Jumlah</td>
-                        <td width="2%">:</td>
-                        <td colspan="3" class="keterangan">
-                            Rp. {{ number_format($trx->jumlah, 2) }}
-                        </td>
+                        <td>Keterangan</td>
+                        <td>:</td>
+                        <td class="keterangan">{{ ucwords($trx->keterangan_transaksi) }}</td>
                     </tr>
                     <tr>
-                        <td width="30%">Kode Akun (D/K)</td>
-                        <td width="2%">&nbsp;</td>
-                        <td colspan="3" class="keterangan">
+                        <td>Jumlah</td>
+                        <td>:</td>
+                        <td class="keterangan">Rp. {{ number_format($trx->jumlah, 2) }}</td>
+                    </tr>
+                    <tr>
+                        <td>Kode Akun (D/K)</td>
+                        <td>:</td>
+                        <td class="keterangan">
                             Debit {{ ucwords($trx->rekening_debit . ' - ' . $trx->rek_debit->nama_akun) }}
                         </td>
                     </tr>
                     <tr>
-                        <td width="30%">&nbsp;</td>
-                        <td width="2%">&nbsp;</td>
-                        <td colspan="3" class="keterangan">
+                        <td></td>
+                        <td></td>
+                        <td class="keterangan">
                             Kredit {{ ucwords($trx->rekening_kredit . ' - ' . $trx->rek_kredit->nama_akun) }}
                         </td>
                     </tr>
                 </table>
+            </div>
 
-                <table width="100%" class="fs-12" style="margin-top: 12px;">
+            <div class="signature-section">
+                <table class="signature-table">
                     <tr>
                         <td align="center">Disetujui,</td>
                         <td align="center">Diverifikasi,</td>
@@ -207,14 +181,7 @@
                         <td align="center">&nbsp;</td>
                     </tr>
                     <tr>
-                        <td align="center">&nbsp;</td>
-                        <td align="center">&nbsp;</td>
-                        <td align="center">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td align="center">&nbsp;</td>
-                        <td align="center">&nbsp;</td>
-                        <td align="center">&nbsp;</td>
+                        <td colspan="3" class="signature-space"></td>
                     </tr>
                     <tr>
                         <td align="center">{{ $dir->namadepan . ' ' . $dir->namabelakang }}</td>
@@ -226,5 +193,4 @@
         </div>
     </div>
 </body>
-
 </html>
