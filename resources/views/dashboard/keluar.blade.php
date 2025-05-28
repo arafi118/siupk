4bfc76953a264bad48644f48dfea6293ead4d82e
yukaristel <rokumitsu012@gmail.com>
2025-05-28 11:45:59 +0700

    keluar

--- resources/views/dashboard/keluar.blade.php
+++ resources/views/dashboard/keluar.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.out')
 
 @section('content')
+<div class="clearfix">
+  <div class="alert alert-danger float-end mt-2 me-2" role="alert" style="max-width: 300px;">
+    Silakan klik <strong><i class="material-icons" style="vertical-align: middle;">exit_to_app</i></strong> di bagian atas menu ini.
+  </div>
+</div>
+
 @endsection
